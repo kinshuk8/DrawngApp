6941db06b3004d76302e547078d416ac48cb8aa5
Kontala Venkat Sai Kinshuk <kinshuksan9@gmail.com>
2023-01-29 20:28:00 +0530

    Basic UI completed

---
+++ app/src/main/java/com/kinari/drawingapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.kinari.drawingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private var drawingView: DrawingView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawing_view)
+        drawingView?.setSizeForBrush(20.toFloat())
+    }
+}
